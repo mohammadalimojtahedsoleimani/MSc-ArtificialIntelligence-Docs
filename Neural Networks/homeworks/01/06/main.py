27b2ab610bc9b2590c47a41ea63848c08e85e449
Mohammad Ali Mojtahed Soleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2024-11-09 23:58:26 +0330

    [ADD] Question 6 of NN homework01

---
+++ Neural Networks/homeworks/01/06/main.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test Accuracy: {test_acc:.4f}')
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train')
+plt.plot(history.history['val_accuracy'], label='Validation')
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train')
+plt.plot(history.history['val_loss'], label='Validation')
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
